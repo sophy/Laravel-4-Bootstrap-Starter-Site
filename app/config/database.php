69cf0cd10c616a40c2e8b31d1ed8d043d1dd5dc6
Sophy Prak <sophy.prak@gmail.com>
2013-05-30 22:19:38 +0700

    Connect ot local DB

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'laravelbootstrapstarter',
-			'username'  => 'user',
-			'password'  => 'password',
+			'database'  => 'gold',
+			'username'  => 'root',
+			'password'  => 'mysqlroot',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
